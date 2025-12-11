04ea1ebcca90d242c95a096660806bb5f820b854
Samuel Baumgartner <samuel.baumgartner@ebmnet.ch>
2025-12-11 19:15:26 +0100

    aödslkjf

--- .next/dev/server/chunks/ssr/_08e423a2._.js
+++ .next/dev/server/chunks/ssr/_08e423a2._.js
@@ -13,7 +13,15 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f2e$pnpm$2f$ne
 ;
 const metadata = {
     title: "Samuel",
-    description: "Samuel Baumgartner"
+    description: "Samuel Baumgartner",
+    icons: {
+        icon: [
+            {
+                url: "/icon.svg",
+                type: "image/svg+xml"
+            }
+        ]
+    }
 };
 function RootLayout({ children }) {
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f2e$pnpm$2f$next$40$16$2e$0$2e$8_$40$babel$2b$core$40$7$2e$28$2e$5_react$2d$dom$40$19$2e$2$2e$1_react$40$19$2e$2$2e$1_$5f$react$40$19$2e$2$2e$1$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("html", {
@@ -23,12 +31,12 @@ function RootLayout({ children }) {
             children: children
         }, void 0, false, {
             fileName: "[project]/src/app/layout.tsx",
-            lineNumber: 16,
+            lineNumber: 21,
             columnNumber: 7
         }, this)
     }, void 0, false, {
         fileName: "[project]/src/app/layout.tsx",
-        lineNumber: 15,
+        lineNumber: 20,
         columnNumber: 5
     }, this);
 }
